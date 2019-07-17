f16ce85203a055378e72d2b505b591c0443ce4a9
Vladimir <x55aah@gmail.com>
2019-07-17 17:58:34 +0300

    updates

--- routes.py
+++ routes.py
@@ -1,11 +1,11 @@
 # For more details about routing, see
 # http://www.tornadoweb.org/en/stable/routing.html
 from anthill.framework.utils.urls import include
-from .api.compat.rest import routes as rest_routes
+from .api.v1.rest import routes as rest_routes
 from tornado.web import url
 from . import handlers
 
-# Create your routes here.
+
 route_patterns = [
-    url(r'^/', include(rest_routes.route_patterns, namespace='api')),  # for compatibility only
+    url(r'^/api/v1', include(rest_routes.route_patterns, namespace='api')),  # for compatibility only
 ]
