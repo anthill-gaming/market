9b73f64096b0a5695a1ae6919422b1b301f88289
Vladimir <x55aah@gmail.com>
2019-08-02 17:03:38 +0300

    update

--- api/v1/rest/routes.py
+++ api/v1/rest/routes.py
@@ -1,9 +1,9 @@
 # For more details about routing, see
 # http://www.tornadoweb.org/en/stable/routing.html
 from tornado.web import url
-from . import handlers
+from . import handlers as h
 
-# Create your routes here.
+
 route_patterns = [
 
 ]
